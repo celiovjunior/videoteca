b9621344940c5eaa46bd8383251bd2909ad7b36d
Celio Junior <celio_cjr@hotmail.com>
2022-06-20 19:25:16 -0300

    solving the cors problem

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const connectToDatabase = require('./database');
+const cors = require('cors');
 require("dotenv").config();
 
 connectToDatabase();
@@ -8,7 +9,7 @@ connectToDatabase();
 const app = express();
 
 const port = 3333;
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
