bb631775fd2838914960197d6ecbf8e49eb17b46
Celio Junior <celio_cjr@hotmail.com>
2022-06-20 09:31:47 -0300

    made the video controller, tests ok

--- server/src/controllers/VideoController.js
+++ server/src/controllers/VideoController.js
@@ -1,1 +1,17 @@
 const Video = require('../models/Video');
+const { response } = require('express');
+
+module.exports = {
+  async index(request, response) {
+    try {
+      const videos = await Video.find();
+
+      return response.status(200).json({ videos });
+
+    } catch (err) {
+
+      response.status(500).json({ error: err.message });
+
+    }
+  }
+};
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
+const VideoController = require('./controllers/VideoController')
+
 routes.get('/', (req, res) => {
   res.send("Hello world!")
 })
+
+routes.get('/videos', VideoController.index)
 
 module.exports = routes;
