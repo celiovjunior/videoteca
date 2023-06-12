cda903d5b11a46af2b9f19c6b5c6d15705b38ee1
Célio V. Júnior <celio_cjr@hotmail.com>
2023-06-12 20:23:29 +1200

    dev: v1 of the project. functionalities are ok

---
+++ formulario.php
@@ -0,0 +1,44 @@
+<?php 
+use PDO;
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+$video = [
+    'url' => '',
+    'title' => '',
+];
+
+if ($id !== false) {
+    $statement = $pdo->prepare("SELECT * FROM videos WHERE id = ?;");
+    $statement->bindValue(1, $id, PDO::PARAM_INT);
+    $statement->execute();
+
+    // $statement->rowCount();
+
+    $video = $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>Enviar video</title>
+</head>
+<body>
+    <h1>Preencha o formulário a seguir para cadastrar um novo vídeo:</h1>
+    <form action=<?= $id !== false ? '/editar-video.php?id=' . $id : '/novo-video.php'; ?> method="post">
+
+        <label for="url">Url do vídeo:</label>
+        <input type="url" name="url" value="<?= $video['url']; ?>">
+
+        <label for="title">Título do vídeo:</label>
+        <input type="text" name="title" id="title" value="<?= $video['title']; ?>">
+
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
---
+++ novo-video.php
@@ -0,0 +1,19 @@
+<?php
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$sql = 'INSERT INTO videos (url, title) VALUES (?, ?)';
+$statement = $pdo->prepare($sql);
+
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+$title = filter_input(INPUT_POST, 'title');
+
+if ($url === false || $title === false) {
+    header("Location: /index.php?sucsess=0");
+    exit();
+}
+
+$statement->bindValue(1, $url);
+$statement->bindValue(2, $title);
+
+$statement->execute();
