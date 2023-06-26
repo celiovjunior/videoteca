6cac94bb08bb6e3a8185897a23e36ca5fd3e6a82
Célio V. Júnior <celio_cjr@hotmail.com>
2023-06-26 19:39:11 +1200

    refactor: set html configs and links into open-html file

--- src/Views/open-html.php
+++ src/Views/open-html.php
@@ -6,3 +6,18 @@
     <link rel="stylesheet" href="./css/styles.css">
     <title>Videoteca</title>
 </head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="logo">
+                <h1>videoteca.</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="./novo-video">New</a></li>
+                    <li><a href="./login">Login</a></li>
+                    <li><a href="./pages/lista-video.html">Sign Up</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
--- src/Views/video-list.php
+++ src/Views/video-list.php
@@ -1,33 +1,18 @@
 <?php
 
 require_once __DIR__ . "/open-html.php"; ?>
-    <body>
-        <header>
-            <div class="navbar">
-                <div class="logo">
-                    <h1>videoteca.</h1>
-                </div>
-                <nav>
-                    <ul>
-                        <li><a href="./novo-video">New</a></li>
-                        <li><a href="./login">Login</a></li>
-                        <li><a href="./pages/lista-video.html">Sign Up</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <section>
-            <ul>
-                <?php foreach ($videoList as $video): ?>
-                    <li class="video-box">
-                        <h3><?= $video->title ?></h3>
-                        <iframe width="350" height="197" src="<?php echo $video->url; ?>" title="<?= $video->title; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                        
-                        <div class="video-box__actions">
-                            <a class="video-box__edit" href="/editar-video?id=<?= $video->id; ?>">Edit</a>
-                            <a class="video-box__delete" href="/remover-video?id=<?= $video->id; ?>">Delete</a>
-                        </div>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-        </section>
+    <section>
+        <ul>
+            <?php foreach ($videoList as $video): ?>
+                <li class="video-box">
+                    <h3><?= $video->title ?></h3>
+                    <iframe width="350" height="197" src="<?php echo $video->url; ?>" title="<?= $video->title; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>                        
+                    <div class="video-box__actions">
+                        <a class="video-box__edit" href="/editar-video?id=<?= $video->id; ?>">Edit</a>
+                        <a class="video-box__delete" href="/remover-video?id=<?= $video->id; ?>">Delete</a>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
 <?php require_once __DIR__ . "/close-html.php";
