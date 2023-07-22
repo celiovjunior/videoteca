053841ca16f25b78c3ec1c73470df517714f3b22
Célio Júnior <celio_cjr@hotmail.com>
2023-07-22 18:59:40 +1200

    feat: created users table; implemented `password_hash` function to hash the password registered a new user by the terminal

---
+++ create-user.php
@@ -0,0 +1,17 @@
+<?php
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+# registrando user atraves do terminal
+$email = $argv[1];
+$password = $argv[2];
+
+# cifrando o password
+$hash = password_hash($password, PASSWORD_ARGON2ID);
+
+$sql = 'INSERT INTO users (email, password) VALUES (?, ?);';
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1, $email);
+$statement->bindValue(2, $hash);
+$statement->execute();
